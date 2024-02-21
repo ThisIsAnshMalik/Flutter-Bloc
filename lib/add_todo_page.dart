302be940be45a8f6000ae832a3117a248ea5a426
Ansh Malik <thisisanshmalik@gmail.com>
2024-02-21 21:30:17 +0530

    Todo with cubit is done

--- lib/add_todo_page.dart
+++ lib/add_todo_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_learning/cubit/todo_cubit.dart';
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -25,7 +27,11 @@ class _AddTodoPageState extends State<AddTodoPage> {
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                BlocProvider.of<TodoCubit>(context)
+                    .addTodo(todoTitleController.text.trim());
+                Navigator.pop(context);
+              },
               child: const Text('Add'),
             ),
           ],
--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -1,6 +1,11 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_learning/models/todo_model.dart';
 
 class TodoCubit extends Cubit<List<TodoModel>> {
   TodoCubit() : super([]);
+
+  void addTodo(String title) {
+    final todo = TodoModel(name: title, createdAt: DateTime.now());
+    emit([...state, todo]);
+  }
 }
