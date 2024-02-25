06a517a0de285ab1815a0a9dcbcc6681127796c6
Ansh Malik <thisisanshmalik@gmail.com>
2024-02-25 17:23:17 +0530

    user Uid fteched from auth bloc using context.watch

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_learning/bloc/auth_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    //context.watch(); will continusly listen changes in the given bloc
+    final authState = context.watch<AuthBloc>().state as AuthSuccess;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(authState.uid),
+      ),
+    );
   }
 }
