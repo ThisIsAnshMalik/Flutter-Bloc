ba11b8b788694260f2cb649e39218a92982c1cde
Ansh Malik <thisisanshmalik@gmail.com>
2024-02-25 18:47:54 +0530

    Weather app UI added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_learning/weather_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const WeatherScreen(),
     );
   }
 }
