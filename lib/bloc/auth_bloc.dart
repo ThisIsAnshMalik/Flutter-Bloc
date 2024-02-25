29ebd1a4e06a18faea3bd4a44b59d3ab743fd858
Ansh Malik <thisisanshmalik@gmail.com>
2024-02-25 18:07:29 +0530

    code improvement

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -6,43 +6,49 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) async {
-      final email = event.email;
-      final password = event.password;
-      emit(AuthLoading());
+    on<AuthLoginRequested>(_onAuthLoginRequest);
+    on<AuthLogOutRequested>(_onLogOutReq);
+  }
 
-      try {
-        String p =
-            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-        RegExp regExp = RegExp(p);
-        // password validation
-        if (!regExp.hasMatch(email)) {
-          emit(const AuthFailure('email is not valid'));
-        }
+  void _onLogOutReq(AuthLogOutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
 
-        // password validation
-        if (password.length < 6) {
-          return emit(const AuthFailure('Password must be 6 Character long'));
-        }
+  void _onAuthLoginRequest(
+      AuthLoginRequested event, Emitter<AuthState> emit) async {
+    final email = event.email;
+    final password = event.password;
+    emit(AuthLoading());
 
-        //on no error
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthSuccess(uid: '$email-$password'));
-        });
-      } catch (e) {
-        return emit(AuthFailure(e.toString()));
+    try {
+      String p =
+          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+      RegExp regExp = RegExp(p);
+      // password validation
+      if (!regExp.hasMatch(email)) {
+        emit(const AuthFailure('email is not valid'));
       }
-    });
 
-    on<AuthLogOutRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthInitial());
-        });
-      } catch (e) {
-        emit(AuthFailure(e.toString()));
+      // password validation
+      if (password.length < 6) {
+        emit(const AuthFailure('Password must be 6 Character long'));
+        return;
       }
-    });
+
+      //on no error
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthSuccess(uid: '$email-$password'));
+      });
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+      return;
+    }
   }
 }
