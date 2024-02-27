cc2747de03a7609d55c9cc88305544b0b23646ba
Ansh Malik <thisisanshmalik@gmail.com>
2024-02-27 20:07:32 +0530

    weather bloc event ready

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc_learning/data/model/weather_data_model.dart';
+import 'package:flutter_bloc_learning/data/repo/weather_repo.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final weatherRepo = WeatherRepo();
+
   WeatherBloc() : super(WeatherInitial()) {
     on<WeatherFetched>(_getCurrentWeather);
   }
 
-  void _getCurrentWeather(WeatherFetched event, Emitter<WeatherState> emiy) {}
+  void _getCurrentWeather(
+    WeatherFetched event,
+    Emitter<WeatherState> emiy,
+  ) async {
+    emit(WeatherLoading());
+    try {
+      final weather = await weatherRepo.getCurrentWeather("London");
+      emit(WeatherSuccess(weatherModel: weather));
+    } catch (e) {
+      emit(WeatherFailure(e.toString()));
+    }
+  }
 }
